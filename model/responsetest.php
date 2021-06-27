7de644a11bd8cb7149cb3e730f9e413ab57c0bc8
Chethan-Gowda <chethan.thimmaiah@gmail.com>
2021-06-27 14:35:27 +0530

    Delete model directory

--- model/responsetest.php
+++
@@ -1,9 +0,0 @@
-<?php
-    require_once('Response.php');
-    $res = new Response();
-    $res->setSuccess(false);
-    $res->setHttpStatusCode(404);
-    $res->addMessage("Error with Message 1");
-    // $res->addMessage("Test Message 2");
-    $res->send();
-?>
